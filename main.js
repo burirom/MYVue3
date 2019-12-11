504773b1125309fdb2cd984fea538ed9398cd729
野村耕秀 <yasu@nomurakoushuunoMacBook-Pro.local>
2019-12-12 01:50:28 +0900

    second commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// メッセージ一覧用コンポーネント
+Vue.component('comp-board', {
+	template: '<div>Message Board</div>',
+})
+// 入力フォーム用コンポーネント
+Vue.component('comp-form', {
+	template: '<div>Form<textarea v-model="message"></textarea></div>',
+	data: function () {
+		return {
+			message: ''
+		}
+	}
+})
+
+new Vue({
+	el: '#app',
+	data: {
+		current: 'comp-board' // 動的に切り替える
+	}
+})
